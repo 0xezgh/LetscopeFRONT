bcc34f808f628c0d15675a79a7f1d0440229cb3a
mahdibenamar <mahdi.benamar@eprit.tn>
2016-04-11 10:39:42 +0100

    Implementation de la page modifier profil + ajout de la méthode Update profil
    ~ /user/edit-profile.html
    ~ /user/ProfileController.js
    ~ /user/UserService.js

--- dataServices/userDataService.js
+++ dataServices/userDataService.js
@@ -1,21 +1,19 @@
 (function(){
 	'use strict';
-	
+
 	angular
-		.module('letscope')
-		.factory('UserDataService',UserDataService);
-	
+			.module('letscope')
+			.factory('UserDataService',UserDataService);
+
 	UserDataService.$inject = ['$resource']
-	
+
 	function UserDataService($resource){
-		var service = {};
-
-		service.GetByUsername = GetByUsername;
-		
-		return service;
-
-		function GetByUsername(){
-			return $resource('http://localhost:3000/user/:username');
-		}
+		return $resource('http://localhost:3000/user/:id',{id: '@id'},
+				{   'get':            {method: 'GET', params: { id: '@id'}},
+					'update':    	  {method: 'PUT'}
+				},
+				{
+					stripTrailingSlashes: false
+				});
 	}
 })();
--- user/userService.js
+++ user/userService.js
@@ -7,31 +7,34 @@
 
     UserService.$inject = ['UserDataService'];
 
-    function UserService (userDataService,Session,AUTH_EVENTS){
+    function UserService (UserDataService,Session,AUTH_EVENTS){
         var service = {};
 
         service.GetProfile = GetProfile;
+        service.UpdateProfile = UpdateProfile;
 
         return service;
 
-        function GetProfile(username,callback){
+        function GetProfile(id,callback){
             var response;
-            userDataService.GetByUsername().get({
-                username: username
+            UserDataService.get({
+                id: id
             },function(user){
                 if(user.error == null)
                 {
+                    console.log(user);
                     response = {
                         success:true,
                         username : user.username,
                         fname : user.name.fname,
                         lname : user.name.lname,
+                        occupation : user.occupation,
                         email : user.email,
                         city : user.city,
                         country : user.country,
-                        aboutme: user.about_me,
-                        myhobbies: user.my_hobbies,
-                        image : user.profile_image_url,
+                        aboutme: user.aboutme,
+                        myhobbies: user.myhobbies,
+                        image : user.profileimageurl,
                         facebook : user.fb.url,
                         twitter : user.twitter.url,
                         google : user.google.url,
@@ -48,7 +51,38 @@
             });
         }
 
-
+        function UpdateProfile(id,fname,lname,occupation,website,country,city,aboutme,myhobbies,facebook,twitter,google,pinterest,instagram,linkedin,callback){
+            var response;
+            console.log('New user = ' + " - " +fname +" - " + lname +" - " + occupation +" - " +website +" - " +country+" - " +city+" - " +aboutme+" - " +myhobbies+" - " +facebook+" - " +twitter+" - " +google+" - " +pinterest+" - " +instagram);
+            UserDataService.update({
+                id : id,
+                fname : fname,
+                lname : lname,
+                occupation : occupation,
+                website : website,
+                country : country,
+                city : city,
+                aboutme : aboutme,
+                myhobbies : myhobbies,
+                facebook : facebook,
+                twitter : twitter,
+                google : google,
+                pinterest : pinterest,
+                instagram : instagram,
+                linkedin : linkedin
+            },function(profile){
+                if(profile.error == null)
+                {
+                    console.log("Done");
+                    response = {success : true};
+                }
+                else{
+                    console.log("!Done");
+                    response = {success : true};
+                }
+                callback(response);
+            });
+        }
 
     }
 
